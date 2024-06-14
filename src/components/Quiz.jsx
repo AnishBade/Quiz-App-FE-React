ec15ea6d8f073468ddd4f667d514c1b413655cf0
Anish Bade <badeanish6045@gmail.com>
2024-06-14 17:35:28 +0545

    adding quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
     function handleSelectAnswer(selecteAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [
@@ -16,6 +18,20 @@ export default function Quiz() {
 
         });
     }
+
+    if (quizIsComplete) {
+        return (
+            <div id='summary'>
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
